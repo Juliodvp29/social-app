2ef07d0682e6876295602c53c5ee8e19a8564d9b
Juliodvp29 <juliodevep29@gmail.com>
2022-06-09 08:41:13 -0500

    inital

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
 import { AuthModule } from './auth/auth.module';
+import { PagesModule } from './pages/pages.module';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AuthModule
+    AuthModule,
+    PagesModule
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
